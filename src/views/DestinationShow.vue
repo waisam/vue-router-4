de8d1de43fee59355b76533b3914ae90ba31feba
waisam <waisam@live.com>
2023-10-25 04:15:46 +0800

    Reacting to Param Changes(响应路由参数变化)

--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,25 +1,25 @@
 <script lang="ts" setup>
-  import { destinations } from '@/data.json'
   // 组合式风格无法访问this.$route, 需要导入`useRoute`函数
   import { useRoute } from 'vue-router'
 
-  // 选项式风格
-  // export default {
-  // computed: {
-  //   destination(): any {
-  //     return destinations.find(destination => destination.slug === this.$route.params.name)
-  //   }
-  // }
-  // 第二种
-  // data() {
-  //   return {
-  //     destination: destinations.find(destination => destination.slug === this.$route.params.name)
-  //   }
-  // }
-  // }
+  // 监听路由参数变量
+  import { ref, watch } from 'vue'
 
-  // 组合式风格
-  const destination = destinations.find(destination => destination.slug === useRoute().params.name)
+  const route = useRoute()
+  const destination: any = ref(null)
+
+  // 监听路由参数的变化
+  watch(
+    () => route.params.name, // 无法直接使用属性作为监听对象，需要将其以getter函数返回值的形式提供给watch
+    async (newValue, oldValue) => {
+      console.info('Goodbye ' + oldValue)
+      // 用变化后的新值组成新的请求路径或作为新的请求参数
+      // let response = await fetch(`https://travel-dummy-api.netlify.app/${newValue}`)
+      let response = await fetch(`http://localhost:5173/data/${newValue}.json`) // 远程数据调用效率太低,用本地数据替换
+      destination.value = await response.json()
+    },
+    { immediate: true } // 载入完成立即触发回调函数
+  )
 </script>
 
 <template>
